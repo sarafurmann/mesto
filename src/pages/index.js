5b29913524e085e9d450e26029e7cb92d0657cc4
Сара Фурманова <sarafurmann@192.168.1.4>
2022-12-27 01:31:08 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,49 @@
+let profileInfoButton = document.body.querySelector(".profile__info-button");
+let profileEditform = document.body.querySelector(".profile__editform");
+let profileEditformCloseButton = profileEditform.querySelector(".popup__btn-close");
+
+profileInfoButton.addEventListener("click", function clickHandler() {
+    profileEditform.classList.add("active");
+})
+
+profileEditformCloseButton.addEventListener("click", function clickHandlerClose() {
+    profileEditform.classList.remove("active");
+})
+
+profileEditform.addEventListener("submit", function handleFormSubmit (evt) {
+    evt.preventDefault();
+    let formData = new FormData(evt.target);
+    let profile = {
+        name: formData.get("name"),
+        job: formData.get("job")
+    }
+
+    renderProfile(profile);
+
+    profileEditform.classList.remove("active");
+})
+
+let profile = {
+    name: "Жак-Ив Кусто",
+    job: "Исследователь океана"
+};
+
+function renderProfile(profile) {
+    let profileName = document.body.querySelector(".profile__info-name");
+    profileName.textContent = profile.name;
+
+    let profileJob = document.body.querySelector(".profile__info-job");
+    profileJob.textContent = profile.job;
+}
+
+function renderProfileForm(profile) {
+    let profileNameAbout = profileEditform.querySelector("#name");
+    profileNameAbout.value = profile.name;
+
+    let profileJobPlace = profileEditform.querySelector("#job");
+    profileJobPlace.value = profile.job;
+}
+
+renderProfile(profile);
+renderProfileForm(profile);
+
