21681556d57184b022df50e4d26bb62c40d19ab3
Сара Фурманова <sarafurmann@Saras-MacBook-Air.local>
2023-03-01 02:06:38 +0300

    7modul

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-import { initialCards, Card } from './Card.js'
+import { Card } from './Card.js'
 import { FormValidator } from './FormValidator.js'
 
 const popups = document.querySelectorAll('.popup');
@@ -26,6 +26,33 @@ const zoomPopup = document.body.querySelector('#element-zoom');
 const zoomPopupCloseBtn = zoomPopup.querySelector('.popup__btn-close');
 
 const formElement = document.body.querySelector('.popup__form');
+
+export const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
 
 profileInfoButton.addEventListener('click', handleClick);
